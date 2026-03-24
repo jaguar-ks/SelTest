d80139d3bf72e7e5bfdb7c6260a8bcd03bec569d
jaguar-ks <fahdamineksouss@gmail.com>
2026-03-24 10:42:29 +0100

    [INIT] : working on a selenium java driver that check github repositories

---
+++ src/main/java/com/seltest/SelTestApplication.java
@@ -0,0 +1,12 @@
+package com.seltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelTestApplication.class, args);
+    }
+}
